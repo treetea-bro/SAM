32448a3955e17eb52655b850ceb065dff099605a
peco <sorpwjdafh@naver.com>
2024-06-05 22:25:42 +0900

    fix(comment): comment for cutout && stepLR

--- example/utility/cutout.py
+++ example/utility/cutout.py
@@ -2,19 +2,29 @@ import torch
 
 
 class Cutout:
+    """랜덤한 위치에 정해진 사각형 크기의 사이즈(원본사이즈보다 작게)로 이미지 색을 검은색으로 변환하여
+    학습 시에 모델이 좀 더 보편적으로 이미지를 학습할 수 있게 도와주는 기법(aka. 일반화)"""
+
     def __init__(self, size=16, p=0.5):
-        self.size = size
+        self.size = size  # 정사각형 한변의 길이
         self.half_size = size // 2
-        self.p = p
+        self.p = p  # 사각형을 삽입할 확률
 
     def __call__(self, image):
+        # 0~1 사이의 랜덤하게 생성된 값이0.51 이상이면 cutout 중지
         if torch.rand([1]).item() > self.p:
             return image
 
-        left = torch.randint(-self.half_size, image.size(1) - self.half_size, [1]).item()
+        # 이미지의 좌측과 위측의 좌표를 랜덤하게 구하고,
+        # 미리 정의된 한변의 길이를 더해서 우측과 아래측의
+        # 좌표를 구한다.
+        left = torch.randint(
+            -self.half_size, image.size(1) - self.half_size, [1]
+        ).item()
         top = torch.randint(-self.half_size, image.size(2) - self.half_size, [1]).item()
         right = min(image.size(1), left + self.size)
         bottom = min(image.size(2), top + self.size)
 
-        image[:, max(0, left): right, max(0, top): bottom] = 0
+        # 구해진 좌표를 가지고 슬라이싱을 이용해 이미지의 일부분을 검은색(0)으로 만든다.
+        image[:, max(0, left) : right, max(0, top) : bottom] = 0
         return image
--- example/utility/step_lr.py
+++ example/utility/step_lr.py
@@ -1,19 +1,24 @@
 class StepLR:
+    """total_epochs 기준으로 특정 구간에 진입 할때마다 점진적으로
+    learning_rate를 감소시켜 minima에 수렴할 수 있도록 도와주는 클래스"""
+
     def __init__(self, optimizer, learning_rate: float, total_epochs: int):
         self.optimizer = optimizer
         self.total_epochs = total_epochs
         self.base = learning_rate
 
     def __call__(self, epoch):
-        if epoch < self.total_epochs * 3/10:
+        if epoch < self.total_epochs * 3 / 10:
             lr = self.base
-        elif epoch < self.total_epochs * 6/10:
+        elif epoch < self.total_epochs * 6 / 10:
             lr = self.base * 0.2
-        elif epoch < self.total_epochs * 8/10:
-            lr = self.base * 0.2 ** 2
+        elif epoch < self.total_epochs * 8 / 10:
+            lr = self.base * 0.2**2
         else:
-            lr = self.base * 0.2 ** 3
+            lr = self.base * 0.2**3
 
+        # 특정구간에서 계산된 learning_rate를
+        # 옵티마이저에 실제로 적용.
         for param_group in self.optimizer.param_groups:
             param_group["lr"] = lr
 
